600db57f29d4ecd4d8554f3413cfcc627133fd2c
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2022-06-16 20:37:04 +0900

    howtoplayLine1

--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -8,6 +8,12 @@ const HowToPlay = () => {
     <div className='howtoplay-panel'>
         <h2 className='how-header'>How To Play?</h2>
         <button className='close-how-button' onClick={()=>setDisplayInformation(false)}><FaTimes/></button>
+        <div className='howtoplay-content'>
+            <div className='howtoplay-lines'>
+              <p className='line1'>Each word guessed must be a valid 6 letter words! After you have guessed the 
+              word hit the ENTER button to submit and check whether the word is present on the list or not!</p>
+            </div>
+        </div>
     </div>
   )
 };
